f8a95aa43836e9e445f165fb1f1f2210f44ebef6
Luca Deminas <lucademinas@gmail.com>
2024-10-28 20:57:00 -0300

    Put y delete en sysadmin

--- src/components/navbar/CommonNavbar.jsx
+++ src/components/navbar/CommonNavbar.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Navbar, Nav, Button, Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from "../../img/Logo-Diego.jpg";
+import { useNavigate } from 'react-router-dom';
 
 const CommonNavbar = () => {
+  const navigate = useNavigate();
   return (
     <Navbar color="light"  variant="light" style={{borderBottom: '2px solid #504f4f'}}>
       <Container>
-        <Navbar.Brand href="#home">
+        <Navbar.Brand href="/">
           <img
             src={logo}
             alt="logo"
@@ -17,8 +19,7 @@ const CommonNavbar = () => {
           />
         </Navbar.Brand>
         <Nav className="ml-auto">
-          {/* Botón de Sign In */}
-          <Button variant="outline-secondary">Sign In</Button>
+          <Button onClick={() => navigate("/login")} variant="outline-secondary">Sign In</Button>
         </Nav>
       </Container>
     </Navbar>
